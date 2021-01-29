3552d70461038759268ab98d459c989c39f83a4e
warlinx <58605680+warlinx@users.noreply.github.com>
2021-01-29 21:31:50 +0300

    Add files via upload
    CRUD

---
+++ CRUD/db.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 23 2020 г., 08:21
+-- Версия сервера: 5.7.25
+-- Версия PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `body` text COLLATE utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `price` bigint(11) DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `products`
+--
+
+INSERT INTO `products` (`id`, `title`, `price`, `description`) VALUES
+(2, 'Product 2', 2000, 'Lorem ipsum 2');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Индексы таблицы `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ CRUD/update.php
@@ -0,0 +1,48 @@
+<?php
+
+    /*
+     * Подключаем файл для получения соединения к базе данных (PhpMyAdmin, MySQL)
+     */
+
+    require_once 'config/connect.php';
+
+    /*
+     * Получаем ID продукта из адресной строки - /product.php?id=1
+     */
+
+    $product_id = $_GET['id'];
+
+    /*
+     * Делаем выборку строки с полученным ID выше
+     */
+
+    $product = mysqli_query($connect, "SELECT * FROM `products` WHERE `id` = '$product_id'");
+
+    /*
+     * Преобразовывем полученные данные в нормальный массив
+     * Используя функцию mysqli_fetch_assoc массив будет иметь ключи равные названиям столбцов в таблице
+     */
+
+    $product = mysqli_fetch_assoc($product);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Update Product</title>
+</head>
+<body>
+    <h3>Update Product</h3>
+    <form action="vendor/update.php" method="post">
+        <input type="hidden" name="id" value="<?= $product['id'] ?>">
+        <p>Title</p>
+        <input type="text" name="title" value="<?= $product['title'] ?>">
+        <p>Description</p>
+        <textarea name="description"><?= $product['description'] ?></textarea>
+        <p>Price</p>
+        <input type="number" name="price" value="<?= $product['price'] ?>"> <br> <br>
+        <button type="submit">Update</button>
+    </form>
+</body>
+</html>
